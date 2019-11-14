7343659fb8b012c727e408d59f57ca405a93f6fd
joaopat98 <joaopat98@gmail.com>
2019-11-14 15:27:33 +0000

    Add base

---
+++ Assets/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cameraPos = transform.position;
+        var playerPos = player.position;
+        cameraPos.y = playerPos.y;
+        transform.position = cameraPos;
+    }
+}
