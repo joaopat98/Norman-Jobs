aa982d346dacb4b1e269c42b8c231fa2804a9f46
dgpereira98 <dgpereira98@gmail.com>
2019-11-18 14:19:44 +0000

    update punch

--- Assets/Punch.cs
+++ Assets/Punch.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Punch : MonoBehaviour
 {
-    void OnCollisionStay2D(Collision2D col)
+    void OnTriggerStay2D(Collider2D col)
     {
         if(col.gameObject.tag == "Enemy" && Input.GetKeyDown(KeyCode.Space))
         {
