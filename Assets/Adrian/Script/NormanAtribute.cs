e762ae84226af4e7579a256645c979dc501c9d1b
Adrian <adrianrodriguesm98@gmail.com>
2019-11-18 14:56:58 +0000

    Pick up

---
+++ Assets/Adrian/Script/NormanAtribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormanAtribute : MonoBehaviour
+{
+    public Weapon weapon;
+
+    void Update()
+    {
+        if(weapon != null)
+        {
+            GameObject thePlayer = GameObject.FindGameObjectWithTag("Player");
+            weapon.transform.position = thePlayer.transform.position;
+        }
+    }
+    public void SetWeapon(Weapon weapon)
+    {
+        this.weapon = weapon;
+    }
+
+
+}
---
+++ Assets/Adrian/Script/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+   
+   
+    // Start is called before the first frame update
+    public float range = 1.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GameObject thePlayer = GameObject.FindGameObjectWithTag("Player");
+        if (Input.GetMouseButtonDown(0) && (thePlayer.transform.position - transform.position).magnitude < range)
+        {      
+            Debug.Log("Tenho arma");
+            
+            NormanAtribute playerScript = thePlayer.GetComponent<NormanAtribute>();
+           
+            playerScript.SetWeapon(Instantiate(this, thePlayer.transform.position, thePlayer.transform.rotation));
+            Destroy(gameObject);
+            
+        }
+    }
+
+}
