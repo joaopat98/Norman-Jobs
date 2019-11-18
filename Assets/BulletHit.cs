e44add52c475d4aef4a0399f0f837f0ff532e71e
joaopat98 <joaopat98@gmail.com>
2019-11-18 14:45:04 +0000

    fix merge issue, enemies now die with bullet

--- Assets/BulletHit.cs
+++ Assets/BulletHit.cs
@@ -7,8 +7,14 @@ public class BulletHit : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (!col.CompareTag("Player") && !col.CompareTag("Bullet"))
+        if (col.CompareTag("Enemy"))
+        {
+            Destroy(col.gameObject);
             Destroy(gameObject);
+        }
+        else if (!col.CompareTag("Player") && !col.CompareTag("Bullet"))
+            Destroy(gameObject);
+
     }
     // Start is called before the first frame update
     void Start()
