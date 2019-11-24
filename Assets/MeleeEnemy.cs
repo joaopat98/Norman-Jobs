4b74b5156dfa39b1247b15a40f7be014a09976f9
Adrian <adrianrodriguesm98@gmail.com>
2019-11-24 15:55:43 +0000

    UnlockDoor e diferentes enemigos

---
+++ Assets/MeleeEnemy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : Enemy
+{
+    
+    // Start is called before the first frame update
+    private float timeBtwPunchs;
+    public float startTimeBtwPubchs;
+
+    public GameObject Punch;
+    public float MeleeSpeed = 1;
+    public Sprite attackSprite;
+    public Sprite normalSprite;
+    public SpriteRenderer sprite;
+    new void Start()
+    {
+        base.Start();
+        timeBtwPunchs = startTimeBtwPubchs;
+        normalSprite = GetComponent<SpriteRenderer>().sprite;
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(player.transform.position, transform.position) > stopDistance)
+        {
+            Vector2 dir = (player.transform.position - transform.position).normalized;
+            this.GetComponent<Rigidbody2D>().velocity = speed * dir * Time.deltaTime;
+
+        }
+        else if (Vector2.Distance(player.transform.position, transform.position) < stopDistance)
+        {
+            this.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+
+            if (timeBtwPunchs <= 0)
+            {
+                Vector2 dir = (player.transform.position - transform.position).normalized;
+               
+                GameObject punch = Instantiate(Punch, transform.position, Quaternion.identity);
+
+               
+
+                punch.GetComponent<Rigidbody2D>().velocity = MeleeSpeed * dir;
+                punch.transform.Rotate(0, Mathf.Atan2(dir.y, dir.x), 0);
+                timeBtwPunchs = startTimeBtwPubchs;
+
+                StartCoroutine(ChangeSprite());
+
+            }
+            else
+            {
+                timeBtwPunchs -= Time.deltaTime;
+            }
+        }
+
+
+    }
+
+    IEnumerator ChangeSprite()
+    {
+        sprite.sprite = attackSprite;
+        yield return new WaitForSeconds(1);
+        sprite.sprite = normalSprite;
+    }
+}
---
+++ Assets/Walls_Unlock/UnlockDoor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockDoor : MonoBehaviour
+{
+    public GameObject enemy;
+    private bool lockDoor;
+    public GameObject door;
+
+    void Start()
+    {
+       
+        lockDoor = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (enemy.transform.childCount <= 0 && lockDoor)
+        {
+            lockDoor = false;
+            Destroy(door);
+        }
+    }
+
+}
