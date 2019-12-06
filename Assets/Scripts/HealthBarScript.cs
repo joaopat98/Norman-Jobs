ce90e9e4a3fa2ab137c2669952b264d3ac9f1009
dgpereira98 <dgpereira98@gmail.com>
2019-12-06 19:11:06 +0000

    Joker start

---
+++ Assets/Scripts/HealthBarScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarScript : MonoBehaviour
+{
+    public Enemy enemy;
+
+    Image healthBar;
+
+    public float maxHP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHP = enemy.HP;
+        healthBar = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.fillAmount = enemy.HP / maxHP;
+    }
+}
