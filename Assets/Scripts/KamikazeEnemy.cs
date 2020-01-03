763d9f076a81d544c786d64c6c242607064c3fdf
Victor Ribeiro <victor.portusil0@gmail.com>
2020-01-03 00:07:27 +0000

    Layout altered, Kamikaze enemy almost done.

---
+++ Assets/Scripts/KamikazeEnemy.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KamikazeEnemy : Enemy
+{
+
+    // Start is called before the first frame update
+    public float AttackRange;
+    public float AttackTime;
+    public float AttackDistance;
+    public float BoxSize = 3.0f;
+    bool IsChasing;
+    public GameObject explosion;
+
+    [Header("Sound Settings")]
+    public float ExplosionSoundVolume;
+    public AudioClip ExplosionSound;
+    public AudioClip BeepSound;
+    
+   
+
+    new void Start()
+    {
+        base.Start();
+        IsChasing = false;
+       
+    }
+
+    // Update is called once per frame
+    new void FixedUpdate()
+    {
+        base.FixedUpdate();
+        
+    }
+
+    protected override void Act()
+    {
+        float dist = Vector2.Distance(player.transform.position, transform.position);
+        Vector2 dir = (player.transform.position - transform.position).normalized;
+
+        Vector2Int dir_ceil = dir.ToSpriteDirection(0.2f);
+
+        if (dir.x > 0)
+        {
+            var scale = transform.localScale;
+            scale.x = -Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }
+        else
+        {
+            var scale = transform.localScale;
+            scale.x = Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }
+
+        if (player != null && dist > stopDistance)
+        {
+            rb.MovePosition(rb.position + (dir * speed * Time.fixedDeltaTime));
+            animator.SetInteger("x", dir_ceil.x);
+            animator.SetInteger("y", dir_ceil.y);
+            IsChasing = true;
+            
+        }
+        if (IsChasing)
+        {
+            
+            StartCoroutine(ExplosionTimer());
+        }
+    }
+
+    protected override void Idle()
+    {
+        rb.velocity = Vector3.zero;
+    }
+
+    private IEnumerator ExplosionTimer()
+    {
+        int temp = 0;
+        float yellowTime = 1.5f;
+        float normalTime = 0.75f;
+        float deltaTime = 0.15f;
+        while (temp < 3 && isAlive())
+        {
+            AudioSource.PlayClipAtPoint(ExplosionSound, this.transform.position, ExplosionSoundVolume);
+            yellowTime -= deltaTime;
+            spr.color = new Color(255, 165, 0, 1);
+            yield return new WaitForSeconds(yellowTime);
+            spr.color = oldColor;
+            yield return new WaitForSeconds(normalTime);
+            temp++;
+        }
+        yield return new WaitForSeconds(0.50f);
+        if (isAlive())
+        {
+            TryExplode();
+            Destroy(gameObject);
+        }
+    }
+
+    public void TryExplode()
+    {
+        AudioSource.PlayClipAtPoint(ExplosionSound, this.transform.position, ExplosionSoundVolume);
+        var ExplosionInstance = Instantiate(explosion, transform.position,Quaternion.identity);
+    }
+
+}
---
+++ Assets/Scripts/KamikazeExplosion.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KamikazeExplosion : MonoBehaviour
+{
+    public float damage = 3.0f;
+    private bool hasExploded = false;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.GetComponent<IHealthSystem>().Hit(gameObject, damage);
+            
+        }
+        
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!hasExploded)
+        {
+            StartCoroutine(RM());
+        }
+              
+    }
+    private IEnumerator RM()
+    {
+        hasExploded = true;
+        yield return new WaitForSeconds(1.0f);
+        Destroy(gameObject);
+    }
+}
