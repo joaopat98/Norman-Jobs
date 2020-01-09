a6e62e931ffb4a124bf58035be261fcf75aa451a
dgpereira98 <dgpereira98@gmail.com>
2020-01-09 21:25:48 +0000

    fix flamethrower, new doors and corridors

--- Assets/FlameBullet.cs
+++ Assets/FlameBullet.cs
@@ -5,9 +5,28 @@ using UnityEngine;
 public class FlameBullet : BulletHit
 {
     public float MaxRangeTime;
+    private float t;
+
+    private Renderer render;
+    private Color color;
+
+    void Start()
+    {
+        t = 0.0f;
+        render = GetComponent<Renderer>();
+        color = render.material.color;
+    }
 
     void Update()
     {
+        t += Time.deltaTime;
+
+        var newColor = color;
+
+        newColor.a = newColor.a * EasingFunction.EaseInCubic(1, 0, t / MaxRangeTime);
+
+        render.material.color = newColor;
+
         Destroy(gameObject, MaxRangeTime);
     }
 
