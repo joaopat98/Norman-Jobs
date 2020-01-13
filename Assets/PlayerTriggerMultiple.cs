260ac908838fc0e8d3005c983e914002c485aa82
joaopat98 <joaopat98@gmail.com>
2020-01-13 21:06:32 +0000

    fix twister only hitting once

---
+++ Assets/PlayerTriggerMultiple.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTriggerMultiple : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float Damage;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            col.gameObject.GetComponent<IHealthSystem>().Hit(gameObject, Damage);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
